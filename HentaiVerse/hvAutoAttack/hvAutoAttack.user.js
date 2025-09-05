0c1c4f69327b6c9e20d96377e4c9de7493a19712
WayneFerdon <wayneferdon@hotmail.com>
2025-09-06 07:24:45 +0800

    释放条件新增可以按照option配置值

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.22.17
+// @version      2.90.22.18
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -1990,7 +1990,7 @@ try {
         let result;
         for (let key of paramList) {
           if (!result) {
-            result = (g('battle') ?? getValue('battle', true))[key] ?? g(key) ?? getValue(key);
+            result = (g('battle') ?? getValue('battle', true))[key] ?? g(key) ?? getValue(key) ?? g('option')?.[key];
             continue;
           }
           result = result[key]
