eb005c493beea8aaceae39f0a0959e3a9b1856ee
WayneFerdon <wayneferdon@hotmail.com>
2025-09-05 12:18:38 +0800

    修正iscd的判断

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.22.14
+// @version      2.90.22.15
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -520,7 +520,7 @@ try {
     if (id * 1 > 10000) { // 使用物品
       return gE(`.bti3>div[onmouseover*="${id}"]`);
     } // 施放技能
-    return (gE(id) && gE(id).style.opacity !== '0.5') ? gE(id) : false;
+    return (gE(id) && gE(id).style.opacity !== 0.5) ? gE(id) : false;
   }
 
   function setLocal(item, value) {
