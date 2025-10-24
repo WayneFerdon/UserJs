cdcb7837462f7be3bfa5d47b798f6d2c39a03c42
WayneFerdon <wayneferdon@hotmail.com>
2025-10-24 20:57:35 +0800

    修正耐力检查条件判断

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.22.25
+// @version      2.90.22.26
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -2658,7 +2658,7 @@ try {
       href = 'ar';
     }
     cost ??= staminaCost[key];
-    if (!checkBattleReady(idleArena, { staminaCost: cost, checkEncounter: true, staminaLow: g('option').staminaGrindFest})) {
+    if (!checkBattleReady(idleArena, { staminaCost: cost, checkEncounter: true, staminaLow: href = 'gr' ? g('option').staminaGrindFest : 0})) {
       logSwitchAsyncTask(arguments);
       return;
     }
