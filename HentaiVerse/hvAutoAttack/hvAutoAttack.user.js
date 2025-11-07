019270dc5ee2c339a216b22ac6ed44ace73f75fe
WayneFerdon <wayneferdon@hotmail.com>
2025-11-07 16:29:43 +0800

    Update hvAutoAttack.user.js

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.33
+// @version      2.90.32
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -3079,7 +3079,6 @@ try {
     const names = g('option').battleOrderName?.split(',') ?? [];
     for (let i = 0; i < names.length; i++) {
       if(taskList[names[i]]()){
-        console.log(names[i]);
         onStepInDone();
         return;
       }
@@ -3186,7 +3185,6 @@ try {
     if(!getValue('stepIn')){
       return;
     }
-    console.trace('onStepInDone');
     delValue('stepIn');
     pauseChange();
   }
