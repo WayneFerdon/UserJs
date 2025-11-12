471c5a349498c45c9def42d49671c55a5400ed04
WayneFerdon <wayneferdon@hotmail.com>
2025-11-12 18:03:30 +0800

    2.90.38 增加使用其他技能前的非空检查

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.37
+// @version      2.90.38
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -2517,6 +2517,7 @@
         if (!perks) {
           return;
         }
+        console.log(perks);
         setValue('staminaHathperk', perks[25].innerHTML.includes('Obtained'));
         $async.logSwitch(arguments);
       } catch (e) { console.error(e) }
@@ -4093,9 +4094,13 @@
         2403: 2,
         1111: 4,
       }
+      const optionSkills = !g('option').skill;
+      if (!optionSkills) {
+        return;
+      }
       for (let i in skillOrder) {
         let skill = skillOrder[i];
-        if (!skill || !g('option').skill[skill]) {
+        if (!skill || !optionSkills[skill]) {
           return;
         }
         let id = skillLib[skill];
