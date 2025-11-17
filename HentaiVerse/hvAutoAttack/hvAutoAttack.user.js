70312f62c09c93aeaccb5725b62f15100ae2b7ea
WayneFerdon <wayneferdon@hotmail.com>
2025-11-18 04:21:38 +0800

    2.90.54 增加设置：压榨届可使用单独的库存检查

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.53
+// @version      2.90.54
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -1040,6 +1040,36 @@
         '  <input id="isCheck_13211" type="checkbox"><input class="hvAANumber" name="checkItem_13211" placeholder="0" type="text"><l0>幻影卷轴</l0><l1>幻影捲軸</l1><l2>Scroll of Shadows</l2>',
         '  <input id="isCheck_13221" type="checkbox"><input class="hvAANumber" name="checkItem_13221" placeholder="0" type="text"><l0>生命卷轴</l0><l1>生命捲軸</l1><l2>Scroll of Life</l2>',
         '  <input id="isCheck_13299" type="checkbox"><input class="hvAANumber" name="checkItem_13299" placeholder="0" type="text"><l0>众神卷轴</l0><l1>眾神捲軸</l1><l2>Scroll of the Gods</l2>',
+        '</div></div>',
+        '  <div><input id="checkSupplyGF" type="checkbox"><b>[C!!]<l0>压榨届使用单独的库存检查</l0><l1>壓榨屆使用單獨的庫存檢查</l1><l2>Standalone supply check for Grind Fest</l2></b>',
+        '  <div class="hvAAcheckItems">',
+        '  <input id="isCheckGF_11191" type="checkbox"><input class="hvAANumber" name="checkItemGF_11191" placeholder="0" type="text"><l0>体力药水</l0><l1>體力藥水</l1><l2>Health Potion</l2>',
+        '  <input id="isCheckGF_11195" type="checkbox"><input class="hvAANumber" name="checkItemGF_11195" placeholder="0" type="text"><l0>体力长效药</l0><l1>體力長效藥</l1><l2>Health Draught</l2>',
+        '  <input id="isCheckGF_11199" type="checkbox"><input class="hvAANumber" name="checkItemGF_11199" placeholder="0" type="text"><l0>体力秘药</l0><l1>體力秘藥</l1><l2>Health Elixir</l2>',
+        '  <input id="isCheckGF_11291" type="checkbox"><input class="hvAANumber" name="checkItemGF_11291" placeholder="0" type="text"><l0>魔力药水</l0><l1>魔力藥水</l1><l2>Mana Potion</l2>',
+        '  <input id="isCheckGF_11295" type="checkbox"><input class="hvAANumber" name="checkItemGF_11295" placeholder="0" type="text"><l0>魔力长效药</l0><l1>魔力長效藥</l1><l2>Mana Draught</l2>',
+        '  <input id="isCheckGF_11299" type="checkbox"><input class="hvAANumber" name="checkItemGF_11299" placeholder="0" type="text"><l0>魔力秘药</l0><l1>魔力秘藥</l1><l2>Mana Elixir</l2>',
+        '  <input id="isCheckGF_11391" type="checkbox"><input class="hvAANumber" name="checkItemGF_11391" placeholder="0" type="text"><l0>灵力药水</l0><l1>靈力藥水</l1><l2>Spirit Potion</l2>',
+        '  <input id="isCheckGF_11395" type="checkbox"><input class="hvAANumber" name="checkItemGF_11395" placeholder="0" type="text"><l0>灵力长效药</l0><l1>靈力長效藥</l1><l2>Spirit Draught</l2>',
+        '  <input id="isCheckGF_11399" type="checkbox"><input class="hvAANumber" name="checkItemGF_11399" placeholder="0" type="text"><l0>灵力秘药</l0><l1>靈力秘藥</l1><l2>Spirit Elixir</l2>',
+        '  <input id="isCheckGF_11501" type="checkbox"><input class="hvAANumber" name="checkItemGF_11501" placeholder="0" type="text"><l0>终极秘药</l0><l1>終極秘藥</l1><l2>Last Elixir</l2>',
+        '  <input id="isCheckGF_19111" type="checkbox"><input class="hvAANumber" name="checkItemGF_19111" placeholder="0" type="text"><l0>花瓶</l0><l1>花瓶</l1><l2>Flower Vase</l2>',
+        '  <input id="isCheckGF_19131" type="checkbox"><input class="hvAANumber" name="checkItemGF_19131" placeholder="0" type="text"><l0>泡泡糖</l0><l1>泡泡糖</l1><l2>Bubble-Gum</l2>',
+        '  <input id="isCheckGF_11401" type="checkbox"><input class="hvAANumber" name="checkItemGF_11401" placeholder="0" type="text"><l0>能量饮料</l0><l1>能量飲料</l1><l2>Energy Drink</l2>',
+        '  <input id="isCheckGF_11402" type="checkbox"><input class="hvAANumber" name="checkItemGF_11402" placeholder="0" type="text"><l0>咖啡因糖果</l0><l1>咖啡因糖果</l1><l2>Caffeinated Candy</l2>',
+        '  <input id="isCheckGF_12101" type="checkbox"><input class="hvAANumber" name="checkItemGF_12101" placeholder="0" type="text"><l0>火焰魔药</l0><l1>火焰魔藥</l1><l2>Infusion of Flames</l2>',
+        '  <input id="isCheckGF_12201" type="checkbox"><input class="hvAANumber" name="checkItemGF_12201" placeholder="0" type="text"><l0>冰冷魔药</l0><l1>冰冷魔藥</l1><l2>Infusion of Frost</l2>',
+        '  <input id="isCheckGF_12301" type="checkbox"><input class="hvAANumber" name="checkItemGF_12301" placeholder="0" type="text"><l0>闪电魔药</l0><l1>閃電魔藥</l1><l2>Infusion of Lightning</l2>',
+        '  <input id="isCheckGF_12401" type="checkbox"><input class="hvAANumber" name="checkItemGF_12401" placeholder="0" type="text"><l0>风暴魔药</l0><l1>風暴魔藥</l1><l2>Infusion of Storms</l2>',
+        '  <input id="isCheckGF_12501" type="checkbox"><input class="hvAANumber" name="checkItemGF_12501" placeholder="0" type="text"><l0>神圣魔药</l0><l1>神聖魔藥</l1><l2>Infusion of Divinity</l2>',
+        '  <input id="isCheckGF_12601" type="checkbox"><input class="hvAANumber" name="checkItemGF_12601" placeholder="0" type="text"><l0>黑暗魔药</l0><l1>黑暗魔藥</l1><l2>Infusion of Darkness</l2>',
+        '  <input id="isCheckGF_13101" type="checkbox"><input class="hvAANumber" name="checkItemGF_13101" placeholder="0" type="text"><l0>加速卷轴</l0><l1>加速捲軸</l1><l2>Scroll of Swiftness</l2>',
+        '  <input id="isCheckGF_13111" type="checkbox"><input class="hvAANumber" name="checkItemGF_13111" placeholder="0" type="text"><l0>守护卷轴</l0><l1>守護捲軸</l1><l2>Scroll of Protection</l2>',
+        '  <input id="isCheckGF_13199" type="checkbox"><input class="hvAANumber" name="checkItemGF_13199" placeholder="0" type="text"><l0>化身卷轴</l0><l1>化身捲軸</l1><l2>Scroll of the Avatar</l2>',
+        '  <input id="isCheckGF_13201" type="checkbox"><input class="hvAANumber" name="checkItemGF_13201" placeholder="0" type="text"><l0>吸收卷轴</l0><l1>吸收捲軸</l1><l2>Scroll of Absorption</l2>',
+        '  <input id="isCheckGF_13211" type="checkbox"><input class="hvAANumber" name="checkItemGF_13211" placeholder="0" type="text"><l0>幻影卷轴</l0><l1>幻影捲軸</l1><l2>Scroll of Shadows</l2>',
+        '  <input id="isCheckGF_13221" type="checkbox"><input class="hvAANumber" name="checkItemGF_13221" placeholder="0" type="text"><l0>生命卷轴</l0><l1>生命捲軸</l1><l2>Scroll of Life</l2>',
+        '  <input id="isCheckGF_13299" type="checkbox"><input class="hvAANumber" name="checkItemGF_13299" placeholder="0" type="text"><l0>众神卷轴</l0><l1>眾神捲軸</l1><l2>Scroll of the Gods</l2>',
         '</div></div>',
         '  </div>',
 
@@ -2431,7 +2461,7 @@
         (async () => { try {
           ready.item = await asyncGetItems() || true;
           await tryEncounter();
-          ready.supply = await asyncCheckSupply();
+          ready.supply = checkSupply();
           await tryEncounter();
         } catch (e) { console.error(e) } })(),
         // repair
@@ -2618,18 +2648,13 @@
       $async.logSwitch(arguments);
     } catch (e) { console.error(e) } }
 
-    async function asyncCheckSupply() { try {
+    function checkSupply(isGFStandalone) {
       if (!g('option').checkSupply) {
         return true;
       }
-      if (getValue('disabled')) {
-        await pauseAsync(_1s);
-        return await asyncCheckSupply();
-      }
-      $async.logSwitch(arguments);
       const items = g('items');
-      const thresholdList = g('option').checkItem;
-      const checkList = g('option').isCheck;
+      const thresholdList = isGFStandalone ? g('option').checkItemGF : g('option').checkItem;
+      const checkList = isGFStandalone ? g('option').isCheckGF : g('option').isCheck;
       const needs = [];
       for (let id in checkList) {
         const item = items[id];
@@ -2645,11 +2670,10 @@
       }
       if (needs.length) {
         console.log(`Needs supply:${needs}`);
-        document.title = `[C!]` + document.title;
+        document.title = `[C!${isGFStandalone ? '!' : ''}]` + document.title;
       }
-      $async.logSwitch(arguments);
       return !needs.length;
-    } catch (e) { console.error(e) } return false; }
+    }
 
     async function asyncCheckRepair() { try {
       if (!g('option').repair) {
@@ -2988,6 +3012,12 @@
         query = 'ar';
       }
       query = `?s=Battle&ss=${query}`;
+
+      if (id === 'gr' && option.checkSupplyGF && !checkSupply(true)) {
+        console.log('Check Battle Ready Failed', 'id:', id, cost);
+        $async.logSwitch(arguments);
+        return;
+      }
       if (!checkBattleReady(idleArena, { staminaCost: cost, checkEncounter: option.encounter, staminaLow: id === 'gr' ? option.staminaGrindFest : undefined })) {
         console.log('Check Battle Ready Failed', 'id:', id, cost);
         $async.logSwitch(arguments);
