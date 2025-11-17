c67a57f07f5cf0d1ad3e7a4cf5adcade52b8619f
WayneFerdon <wayneferdon@hotmail.com>
2025-11-17 14:43:07 +0800

    2.90.46 修正数据记录对旧数据的兼容问题；增加非空检查：战斗中途更新脚本时可能因技能数据未能获取导致报错

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.45
+// @version      2.90.46
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -639,7 +639,7 @@
     function getKeys(objArr, prop) {
       let out = [];
       objArr.forEach((_objArr) => {
-        out = prop ? out.concat(Object.keys(_objArr[prop])) : out.concat(Object.keys(_objArr));
+        out = prop ? out.concat(Object.keys(_objArr[prop])) : _objArr ? out.concat(Object.keys(_objArr)) : out;
       });
       out = out.sort();
       for (let i = 1; i < out.length; i++) {
@@ -4323,7 +4323,7 @@
         if (!ranges) {
           continue;
         }
-        if (ability) {
+        if (ability && ability[ab]) {
           range = ranges[ability[ab].level];
         }
         break;
