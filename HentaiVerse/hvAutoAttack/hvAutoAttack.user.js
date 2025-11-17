72c6a12bd79fc2673962039c1074b6855214794b
WayneFerdon <wayneferdon@hotmail.com>
2025-11-17 14:58:43 +0800

    2.90.48

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.47
+// @version      2.90.48
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -1404,7 +1404,7 @@
               getKeys(statsOld, i).forEach((key) => {
                 _html = `${_html}<tr><td>${key}</td>`;
                 statsOld.forEach((_statsOld) => {
-                  if (key in _statsOld[i]) {
+                  if (_statsOld[i] && (key in _statsOld[i])) {
                     _html = `${_html}<td>${_statsOld[i][key]}</td>`;
                   } else {
                     _html = `${_html}<td></td>`;
