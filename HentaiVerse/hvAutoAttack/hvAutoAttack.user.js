8da29b9ae404e3aa3b657a4ec26feb52003fd7a2
WayneFerdon <wayneferdon@hotmail.com>
2025-11-22 16:11:27 +0800

    2.90.68 Ajax skip duplication fetch requst

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.66
+// @version      2.90.68
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -143,6 +143,11 @@
           return list;
         },
         add: function (method, url, data, onload, onerror, context = {}, headers = {}) {
+          const isDuplicate = this.queue.some(req => req.method === method && req.url === url && JSON.stringify(req.data) === JSON.stringify(data));
+          if (isDuplicate) {
+            console.log('Skip duplicated fetch:', url, method, data);
+            return;
+          }
           method = !data ? 'GET' : method ?? 'POST';
           if (method === 'POST') {
             headers['Content-Type'] ??= 'application/x-www-form-urlencoded';
