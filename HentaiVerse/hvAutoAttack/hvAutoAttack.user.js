5450f83c45bf70c03dead4ba5dc718c02092b930
WayneFerdon <wayneferdon@hotmail.com>
2025-12-05 12:20:58 +0800

    2.90.94 修正debuff Exclusive的非空检查

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.93
+// @version      2.90.94
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -4499,7 +4499,7 @@
             continue;
           }
         }
-        let succeed = useDebuffSkill(buff, isToAll, isToAll && exclusiveBuffs.includes(buff) ? exclusiveBuffs : undefined);
+        let succeed = useDebuffSkill(buff, isToAll, isToAll && exclusiveBuffs?.includes(buff) ? exclusiveBuffs : undefined);
         // 前 toAllCount 个都是先给全体上的
         if (succeed) {
           return true;
