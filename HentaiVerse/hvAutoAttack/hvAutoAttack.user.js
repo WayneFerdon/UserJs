658505e27e6fe35b3e8ebc6eebdc2def4825e0fe
WayneFerdon <wayneferdon@hotmail.com>
2025-12-22 22:12:10 +0800

    2.90.117 修正自动灵动架势的执行

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.116
+// @version      2.90.117
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -3803,8 +3803,8 @@
         'Infus': useInfusions,
         'Debuff': useDeSkill,
         'Focus': autoFocus,
-        'SS': ()=>autoSS(true),
-        'SSDisable': ()=>autoSS(false),
+        'SS': ()=>autoSS(false),
+        'SSDisable': ()=>autoSS(true),
         'Skill': autoSkill,
         'Atk': attack,
       };
