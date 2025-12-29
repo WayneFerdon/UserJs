23c063a17701c8d67cd79248beee4babde7e773d
WayneFerdon <wayneferdon@hotmail.com>
2025-12-29 22:28:49 +0800

    2.90.128 调整attackStatusCurrent的判断流程

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.127
+// @version      2.90.128
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -5196,10 +5196,12 @@
         for (const status of attackStatusOrder) {
           const condition = option[`attackStatusSwitchCondition${status}`] ?? {};
           if (!option.attackStatusSwitch[status]) continue;
-          if (!checkCondition(condition, monsters)) continue;
+          const current = g('attackStatusCurrent');
           g('attackStatusCurrent', status);
-          if (!onAttack(range, status, selectStatusOnly)) continue;
-          return true;
+          if (checkCondition(condition, monsters) && onAttack(range, status, selectStatusOnly)) {
+            return true;
+          }
+          g('attackStatusCurrent', current);
         }
       }
       g('attackStatusCurrent', 0);
