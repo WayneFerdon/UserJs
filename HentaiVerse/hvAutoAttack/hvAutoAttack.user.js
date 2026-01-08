fa6b9cb4b7f7a189d3698ceab61d7413246ed85b
WayneFerdon <wayneferdon@hotmail.com>
2026-01-08 11:49:09 +0800

    2.90.135 修正targetRank的获取方式

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.134
+// @version      2.90.135
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -2917,7 +2917,7 @@
           return getBuffTurnFromImg(getMonsterBuff(getMonsterID(target), img));
         },
         targetRank() {
-          return g('battle').monsterStatus.indexOf(target);
+          return Object.entries(g('battle').monsterStatus).find(([k, v]) => v.order === target.order)[0] * 1;
         },
         targetHp() {
           return target.hpNow / target.hp;
