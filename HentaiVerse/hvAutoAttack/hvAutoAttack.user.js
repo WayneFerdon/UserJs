33700810300bc21e3a63617cf23ab0b71a762591
WayneFerdon <wayneferdon@hotmail.com>
2026-01-14 20:31:40 +0800

    2.90.144 fix bugs in targetName and targetBossType

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.143
+// @version      2.90.144
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -2936,11 +2936,11 @@
         },
         targetName() {
           const mon = getMonster(target.order+1)
-          return gE(`${monsterStateKeys.lv}[style*="background"]`, mon) ? gE(`.btm3>div>div`, mon).innerText : undefined;
+          return gE(`.btm3>div>div`, mon).innerText.replace(' ', '_');
         },
         targetBossType() {
           const name = func.targetName();
-          switch(name) {
+          switch(name.replace('_', ' ')) {
             case 'Manbearpig':
             case 'White Bunneh':
             case 'Mithra':
