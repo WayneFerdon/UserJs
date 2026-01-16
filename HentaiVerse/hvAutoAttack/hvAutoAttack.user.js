5def8ebf6939e32c8df347a11fa7c2bf6e9ed5c8
WayneFerdon <wayneferdon@hotmail.com>
2026-01-16 12:36:11 +0800

    2.90.147 修正迁移skillOTOS后读取解析为object时的问题

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.146
+// @version      2.90.147
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -2833,6 +2833,9 @@
               result = (g('battle') ?? getValue('battle', true))[key] ?? g(key) ?? getValue(key) ?? g('option')?.[key];
               isInData = result;
               continue;
+            }
+            if (typeof result === 'string') {
+              result = JSON.parse(result)
             }
             result = result[key]
           }
