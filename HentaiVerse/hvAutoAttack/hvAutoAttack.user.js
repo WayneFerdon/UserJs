4d4b3226a7b61fc5c695260ba40c646b9b6199ea
WayneFerdon <wayneferdon@hotmail.com>
2026-02-02 14:16:35 +0800

    2.90.159 fix stamina ratio calculation

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.158
+// @version      2.90.159
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -3457,8 +3457,14 @@
         return
       }
       stamina.time = time(0);
-      if (!stamina.punish && stamina.ratio !== 1) {
-        stamina.ratio = 1;
+      if (!stamina.punish) {
+        if (stamina.ratio !== 1) {
+          stamina.ratio = 1;
+        } else {
+          stamina.lastRatio = 1;
+          stamina.lastRatioRaw = undefined;
+          stamina.ratioRaw = undefined;
+        }
         setValue('stamina', stamina);
       }
       const lastCost = stamina.lastCost;
