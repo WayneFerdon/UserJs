3c9abb01ea98440bfbf1e64fc428ef9a4ac38ff8
WayneFerdon <wayneferdon@hotmail.com>
2026-03-05 19:34:09 +0800

    2.90.166 修正公式变量值获取及判定

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.165
+// @version      2.90.166
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -2939,22 +2939,22 @@
         for (let key of paramList) {
           if (typeof result === 'undefined') { // 获取顶层数据
             result = g('battle') ? g('battle')[key] : undefined;
-            if (typeof result === 'undefined') {
+            if (typeof result === 'undefined' || result === null) {
               result = getValue('battle', true) ? getValue('battle', true)[key] : undefined;
             }
-            if (typeof result === 'undefined') {
+            if (typeof result === 'undefined' || result === null) {
               result = g(key);
             }
-            if (typeof result === 'undefined') {
+            if (typeof result === 'undefined' || result === null) {
               result = getValue(key);
             }
-            if (typeof result === 'undefined') {
+            if (typeof result === 'undefined' || result === null) {
               result = g('option')?.[key];
             }
-            if (typeof result === 'undefined' && func[key]) {
+            if ((typeof result === 'undefined' || result === null) && func[key]) {
               result = func[key]();
             }
-            if (typeof result === 'undefined') break;
+            if (typeof result === 'undefined' || result === null) break;
             isInData = true; // 存在顶层数据
             continue;
           }
