dcbe5bed5bad1b145093a576ef85551733efea94
WayneFerdon <wayneferdon@hotmail.com>
2026-04-15 16:14:04 +0800

    Update version number to 2.90.177

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.176
+// @version      2.90.177
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -5386,6 +5386,12 @@
           name: 'Arcane Focus',
           id: '432',
           img: 'arcanemeditation',
+        },
+
+        CF: {
+          name: 'Cloak of the Fallen',
+          id: getBuff('sparklife') ? undefined : 422,
+          img: 'fallenshield',
         }
       };
       if (option.channelSkill) {
