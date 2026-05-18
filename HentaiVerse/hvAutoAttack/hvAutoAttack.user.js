1fb1298269eefb54c1d849e1dcba02c5c46de15a
WayneFerdon <wayneferdon@hotmail.com>
2026-05-18 23:39:06 +0800

    2.90.195 修正技能範圍數據(dodying/UserJs/issues/192#issuecomment-4475530317)
    Updated version number and added range property to monster data.

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.194
+// @version      2.90.195
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -6231,7 +6231,7 @@
         2203: { oc: 4, },
         2302: { range: 5 },
         2303: { range: 5 },
-        2403: { oc: 3, }
+        2403: { oc: 3, range: 5 },
       }
       const monsterStatus = g().battle.monsterStatus;
       for (let i in skillOrder) {
