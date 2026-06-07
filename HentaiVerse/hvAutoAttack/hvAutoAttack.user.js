5eabf89c1041a7b357ed7cf812a3fdf207aa69ed
WayneFerdon <wayneferdon@hotmail.com>
2026-06-08 04:45:45 +0800

    2.90.209 调整targetGroup的默认分组为null而不是undefined

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.208
+// @version      2.90.209
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -3646,7 +3646,7 @@
           return g().oc / 100;
         },
       };
-      let currentGroup;
+      let currentGroup=null;
       const func = {
         ...returnValueGetter.prototype.func,
         targetBuffTurn(...img) {
@@ -3790,7 +3790,7 @@
             default:
               throw new Error(`Unsupported targetGroup mode: ${groupMode}.`);
           }
-          return Object.keys(currentGroup).length;
+          return currentGroup ? 0 : Object.keys(currentGroup).length;
         }
       };
 
