53fe5537fda71d4b3de12a043280835dee3d97b3
WayneFerdon <wayneferdon@hotmail.com>
2026-06-08 05:01:55 +0800

    2.90.210 修正返回值错误

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.90.209
+// @version      2.90.210
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -3790,7 +3790,7 @@
             default:
               throw new Error(`Unsupported targetGroup mode: ${groupMode}.`);
           }
-          return currentGroup ? 0 : Object.keys(currentGroup).length;
+          return currentGroup ? Object.keys(currentGroup).length : 0;
         }
       };
 
