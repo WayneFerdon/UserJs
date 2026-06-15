72c7dd7930bae062d4571386b85029169baa565a
WayneFerdon <wayneferdon@hotmail.com>
2026-06-15 12:28:09 +0800

    2.91.15 Fix Battle Unresponsive

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.14
+// @version      2.91.15
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -5247,7 +5247,7 @@
         'Alt': { method: gotoAlt }
       }
       let min = Infinity;
-      for (let t in option.battleUnresponsive) {
+      for (let t in battleUnresponsive) {
         if (!option.battleUnresponsive[t] || !battleUnresponsive[t]) {
           delete battleUnresponsive[t];
           continue;
