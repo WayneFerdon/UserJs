42589c86902e838d6693a94b82288b70876a0790
WayneFerdon <wayneferdon@hotmail.com>
2026-07-16 18:36:49 +0800

    2.91.39 remove debug console log

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.38
+// @version      2.91.39
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -5286,7 +5286,6 @@
 
     function getBuffTurnFromImg(buff) {
       let duration = matchBuffImg(buff)?.[2];
-      console.log(buff,duration)
       switch (true) {
         case !duration:
           return 0;
