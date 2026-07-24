3603e5b56f27da8aac0aef95830a3decffd1438a
WayneFerdon <wayneferdon@hotmail.com>
2026-07-24 18:23:31 +0800

    2.91.55 Fix buff

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.54
+// @version      2.91.55
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -6420,7 +6420,7 @@
       const buffObj = getBuff(playerBuffSkillLib[buff].img);
       const threshold = option.channelThreshold ? option.channelThreshold[buff] : 0;
       const current = getBuffTurnFromImg(buffObj);
-      const checked = isOn(id) && (current === Infinity || threshold >= 0 && current > threshold);
+      const checked = !isOn(id) || (current === Infinity || threshold >= 0 && current > threshold);
       return { id, buffObj, threshold, current, checked };
     }
 
