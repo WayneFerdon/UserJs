965806fd04eac9fb45de766deb211ee3ccf7d368
WayneFerdon <wayneferdon@hotmail.com>
2026-07-25 03:21:47 +0800

    Update hvAutoAttack.user.js

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.57
+// @version      2.91.58
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -3168,6 +3168,7 @@
 
       function formatOption(option, skipUI) {
         for (const obj in option) {
+          if (['auto', 'server'].includes(obj)) continue;
           if (gE(`[name="${obj}"],[id="${obj}"]`, optionBox)) continue;
           if (option[obj] instanceof Object) {
             let found = false;
