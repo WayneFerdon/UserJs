a6801cb97ef5804a116f46370aa0d70d8d70e2fb
WayneFerdon <wayneferdon@hotmail.com>
2026-07-27 21:18:05 +0800

    Update hvAutoAttack.user.js

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.73
+// @version      2.91.74
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -5216,7 +5216,7 @@
         arena.array = splitOrders(option.idleArenaValue).map(String);
         arena.array.reverse();
       }
-      arena.arrayDone = arena.arrayDone.filter(id => id && id === 'gr' || !arena.enabled?.includes(id.toString()));
+      arena.arrayDone = arena.arrayDone.filter(id => id && (id === 'gr' || !arena.enabled?.includes(id.toString())));
       $async.logSwitch(arguments);
       return setValue('arena', arena);
     } catch (err) { console.error(err); }}
