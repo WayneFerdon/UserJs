50d456a48155e451297b3a53eaf6323e82a341b8
WayneFerdon <wayneferdon@hotmail.com>
2026-07-31 11:44:05 +0800

    2.91.86 Loop audio pause only by both focused and then mousemoved

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.85
+// @version      2.91.86
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -3876,7 +3876,17 @@
         const battleNow = unsafeWindow.battle;
         (async ()=> {
           const start = time(0);
-          await until(()=> document.hasFocus() || (unsafeWindow.battle !== battleNow));
+          let focused;
+          await until(()=> {
+            if (!focused && document.hasFocus()) {
+              focused = true;
+              document.addEventListener('mousemove', () => {
+                audio.pause();
+                audio.loop = false;
+              }, true);
+            }
+            return !audio.loop || (unsafeWindow.battle !== battleNow);
+          });
           audio.pause();
         })();
       }
