f1f28101e16b53e64b36521fecc85195e81b0514
WayneFerdon <wayneferdon@hotmail.com>
2026-08-14 08:32:32 +0800

    2.91.97 Fix hvVersion

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.96
+// @version      2.91.97
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -6326,7 +6326,7 @@
       let monsterStateKeys = ${JSON.stringify(monsterStateKeys)};
       let ability = ${JSON.stringify(ability)};
       let monsterBuffSkillLib = ${JSON.stringify(monsterBuffSkillLib)};
-      let hvVersion = Version(${hvVersion.ver.split('.')});
+      let hvVersion = Version(...${JSON.stringify(hvVersion.ver.split('.'))});
       // funciton
       ${[updateMonsterEffects, fixMonsterStatus,
          getMonsterID, getMonster, getMonster, getBuff,
