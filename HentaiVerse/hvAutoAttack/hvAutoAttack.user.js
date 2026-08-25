53512ca419970e493b03a6048e33cc0c9b5c97b5
WayneFerdon <wayneferdon@hotmail.com>
2026-08-25 17:14:14 +0800

    2.91.124 Idle delay count down display

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.123
+// @version      2.91.124
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -5068,12 +5068,17 @@
       return changed;
     }
 
+    function remainTime2Str(r, byLang, reverseDetail) {
+      const digits = Math.max(2, r >= _1h ? 3 : 2);
+      let str = `${Math.floor(r / _1s)}`;
+      const detail = timeStr(r, digits);
+      str = reverseDetail ? `${detail} (${str})` : `${str} (${detail})`;
+
+      return byLang ? ` ${UI.byLang('剩余', '剩餘', 'Remain')}${str}` : ` ${UI.l('剩余', '剩餘', 'Remain')}${str}`;
+    }
+
     async function displayCDRemain() { try {
       const option = getOption();
-      const time2Str = r => {
-        const digits = Math.max(2, r >= _1h ? 3 : 2);
-        return ` ${UI.l('剩余', '剩餘', 'Remain')}${Math.floor(r / _1s)} (${timeStr(r, digits)})`;
-      }
       await until(() => {
         let idleStarted;
         const idleStart = g().idleStart;
@@ -5089,12 +5094,12 @@
           const v = start + (wait ?? 0) * _1s;
           const r = Math.floor(Math.max(0, v - now) / _1s) * _1s;
           if (k === 'onIdle') idleStarted = r <= 0;
-          return [k, time2Str(r)];
+          return [k, remainTime2Str(r)];
         }));
         gE('.onIdleRemain', 'all').forEach(r => { r.innerHTML = remain.onIdle; });
         if (!idleStarted) {
-          gE('.arenaRemain').innerHTML = time2Str(durations.arena.wait * _1s);
-          gE('.isekaiSwitchRemain').innerHTML = time2Str(durations.switch.wait * _1s);
+          gE('.arenaRemain').innerHTML = remainTime2Str(durations.arena.wait * _1s);
+          gE('.isekaiSwitchRemain').innerHTML = remainTime2Str(durations.switch.wait * _1s);
           gE('.isekaiCDRemain').innerHTML = remain.switchCD;
           return false;
         } else {
@@ -5116,7 +5121,15 @@
       let option = getOption(true);
       const beforeIdle = g('beforeIdle', time(0));
       displayCDRemain();
-      if (!onIsekaiEncounter && option.onIdleDelay) await pauseAsync(option.onIdleDelay * _1s);
+      if (!onIsekaiEncounter && option.onIdleDelay) {
+        const delay = option.onIdleDelay * _1s;
+        until(() => {
+          const remain = beforeIdle + delay - time(0);
+          if (remain <= 0) return true;
+          displayProcess(`${UI.byLang('等待进入闲置', '等待進入閒置', 'Wait for enter idle')}${remainTime2Str(remain, true, true)}`);
+        });
+        await pauseAsync(option.onIdleDelay * _1s);
+      }
       const idleStart = g('idleStart', time(0));
       $async.logSwitch(arguments);
       await updateEncounter(false);
