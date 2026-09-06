96cab242169cbca43674e189fa95e9648ff953a7
WayneFerdon <wayneferdon@hotmail.com>
2026-09-06 08:34:51 +0800

    2.91.191 Log and record opt.

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.190
+// @version      2.91.191
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -8736,14 +8736,14 @@ text-align: left;
     }
 
     function logWarn(info, text, prev, handled, mode) {
-      const toWarn = t => t?.replaceAll(/MONSTER_\d/g, 'MONSTER_0').replaceAll(/ \d+/g, ' 0').replaceAll(/_\d+/g, '_0');
+      const toWarn = t => t?.replaceAll(/MONSTER_\d/g, 'MONSTER_0').replaceAll(/ [\d.]+/g, ' 0').replaceAll(/_\d+/g, '_0');
       const warn = {
         prev: toWarn(prev),
         text: toWarn(text),
         handled: handled.map(toWarn),
         mode
       }
-      if (!logCache.some(w => JSON.stringify(w) === JSON.stringify(warn))) {
+      if (!logCache.some(w => JSON.stringify(w.text) === JSON.stringify(warn.text))) {
         console.warn(info, ':', warn.text);
         logCache.unshift(warn);
       }
@@ -8846,7 +8846,7 @@ text-align: left;
             recorder: () => {
               if (!filter.proficiency) return;
               let [elem, point] = [match[2],match[1]];
-              stats.proficiency[elem] = ((stats.proficiency[elem] ?? 0) + point * 1).toFixed(3);
+              stats.proficiency[elem] = ((stats.proficiency[elem] ?? 0) * 1 + point * 1).toFixed(3);
             }
           },
           {
