3e46928edb41abfaeb065ebccef6fbe78b4ce425
WayneFerdon <wayneferdon@hotmail.com>
2026-09-09 16:28:23 +0800

    2.91.213 Fix Idle arena

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.212
+// @version      2.91.213
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -6370,7 +6370,7 @@ function HVAA(forIsekaiEncounter) {
     }
     $async.logSwitch(arguments);
     if (!arena.data.enabled?.length) {
-      arena.data.enabled = (await updateArena(true)).enabled;
+      arena.data.enabled = (await updateArena(true)).data.enabled;
       setIfChanged('arena', arena);
     }
     while (array.length > 0) {
@@ -6408,7 +6408,7 @@ function HVAA(forIsekaiEncounter) {
       }
       if (arena.data.enabled.includes(id)) break;
       if (id >= 105) {
-        arena.data.enabled = (await updateArena(true)).enabled;
+        arena.data.enabled = (await updateArena(true)).data.enabled;
         setIfChanged('arena', arena);
         if (arena.data.enabled.includes(id)) break;
       }
