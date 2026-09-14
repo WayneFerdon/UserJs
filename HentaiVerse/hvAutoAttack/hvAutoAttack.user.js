57c4515843a9f9b9a53a5414094846aa52ff3eff
WayneFerdon <wayneferdon@hotmail.com>
2026-09-14 11:07:23 +0800

    #208

--- HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
+++ HentaiVerse/hvAutoAttack/hvAutoAttack.user.js
@@ -6,7 +6,7 @@
 // @description  HV auto attack script, for the first user, should configure before use it.
 // @description:zh-CN HV自动打怪脚本，初次使用，请先设置好选项，请确认字体设置正常
 // @description:zh-TW HV自動打怪腳本，初次使用，請先設置好選項，請確認字體設置正常
-// @version      2.91.233
+// @version      2.91.234
 // @author       dodying
 // @namespace    https://github.com/dodying/
 // @supportURL   https://github.com/dodying/UserJs/issues
@@ -1080,7 +1080,7 @@
     }
 
     function cacheIWorTW() {
-      if (isInBattle()) return;
+      if (isInBattle() || forIsekaiEncounter) return;
       switch (_query.s) {
           case 'Bazaar': {
             if (_query.ss !== 'am') return;
